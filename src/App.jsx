63de9934a3ccc337fe540de0bbfaf8ec4581156d
dominik791 <dominik.broj@gmail.com>
2018-08-25 19:16:38 +0200

    clean stuff

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './App.module.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.app}>
+          blabla
+        </div>
+        <div className={styles.name}>bbbb</div>
+      </div>
+    );
+  }
+}
+
+export default App;
